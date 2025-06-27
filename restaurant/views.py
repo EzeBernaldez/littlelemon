c9c2aaede3e77edd813e69afae74818a6203f772
EzeBernaldez <ezebernaldez@gmail.com>
2025-06-27 15:39:27 -0300

    djoser and authentication users

--- restaurant/views.py
+++ restaurant/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework import generics, viewsets
 from rest_framework.permissions import IsAuthenticated
-from .serializers import MenuSerializer, BookingSerializer
+from .serializers import MenuSerializer, BookingSerializer, UserSerializer
 from .models import Menu, Booking
-
+from rest_framework.decorators import api_view, permission_classes
+from django.contrib.auth.models import User
+from rest_framework.response import Response
 
 # Create your views here.
 
@@ -14,6 +16,7 @@ def index(request):
 class MenuItemView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
@@ -25,3 +28,10 @@ class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
     permission_classes = [IsAuthenticated]
+
+
+@api_view()
+def user_list(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users,many=True)
+    return Response(serializer.data)
